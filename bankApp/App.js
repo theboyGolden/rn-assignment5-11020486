824b0925b7df0c1b99d6d7ddcb8a10af15be0f3e
theboyGolden <preciousstick15@gmail.com>
2024-06-25 16:18:39 +0000

    bottom tab navigation done

---
+++ bankApp/App.js
@@ -0,0 +1,95 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Home, MyCards, Statistics, Settings } from './screens';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+const Tab = createBottomTabNavigator()
+const screenOptions = {
+  tabBarShowLabel: false,
+  headerShown: false,
+  tabBarStyle: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    left: 0,
+    elevation: 0,
+    height: 60,
+    background: '#fff'
+  }
+}
+export default function App() {
+  return (
+    <NavigationContainer screenOptions={screenOptions}>
+      <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen 
+        name="Home" 
+        component={Home} 
+        options={{
+          tabBarIcon: ({focused})=>{ 
+            return ( 
+              <View style={{alignItems: 'center', justifyContent: 'center'}}>
+                <Image source={require('./assets/home.png')} style={{height: 30, width:30, tintColor: focused ? 'blue' : '#27272A'}} /> 
+                 <Text style={{fontSize: 12, color: focused ? 'blue' : '#212121'}}>Home</Text> 
+              </View>
+              )
+              }
+            }}
+            />
+        <Tab.Screen 
+          name="My Cards" 
+          component={MyCards}
+          options={{
+            tabBarIcon: ({focused})=>{ 
+              return ( 
+                <View style={{alignItems: 'center', justifyContent: 'center'}}>
+                  <Image source={require('./assets/wallet.png')} style={{height: 30, width:30, tintColor: focused ? 'blue' : '#27272A'}} /> 
+                  <Text style={{fontSize: 12, color: focused ? 'blue' : '#212121'}}>My Cards</Text>
+                </View>
+                )
+                }
+              }} 
+              />
+        <Tab.Screen 
+          name="Statistics" 
+          component={Statistics}
+          options={{
+            tabBarIcon: ({focused})=>{ 
+              return ( 
+                <View style={{alignItems: 'center', justifyContent: 'center'}}>
+                  <Image source={require('./assets/pie-chart.png')} style={{height: 30, width:30, tintColor: focused ? 'blue' : '#27272A'}} /> 
+                  <Text style={{fontSize: 12, color: focused ? 'blue' : '#212121'}}>Statistics</Text>
+                </View>
+                )
+                }
+              }}
+              />
+        <Tab.Screen 
+         name="Settings" 
+         component={Settings}
+         options={{
+          tabBarIcon: ({focused})=>{ 
+            return ( 
+              <View style={{alignItems: 'center', justifyContent: 'center'}}>
+                <Image source={require('./assets/setting.png')} style={{height: 30, width:30, tintColor: focused ? 'blue' : '#27272A'}} /> 
+                <Text style={{fontSize: 12, color: focused ? 'blue' : '#212121'}}>Settings</Text>
+              </View>
+              )
+              }
+            }} 
+            />
+      </Tab.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ bankApp/screens/Home.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <View style={{flex:1, alignItems: "center", justifyContent: "center"}}>
+      <Text>Home</Text>
+      <Text>HELLO FUCKING FREAKING WORLD</Text>
+    </View>
+  )
+}
